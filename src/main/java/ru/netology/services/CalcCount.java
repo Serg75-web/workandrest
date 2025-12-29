4528c549672e63ccf0a76a3b2093a4410a58de6c
Serg75 <activequest@gmail.com>
2025-12-29 08:35:39 +0300

    change

--- src/main/java/ru/netology/services/CalcCount.java
+++ src/main/java/ru/netology/services/CalcCount.java
@@ -5,7 +5,7 @@ public class CalcCount {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // имеющиеся средства
-        int t = money-expenses; // имеющиеся средства за вычетом обязательной траты
+        int t = money - expenses; // имеющиеся средства за вычетом обязательной траты
 
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) { // можем ли отдыхать?
